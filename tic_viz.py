97d1f037c74ec987014ed19ef0e08de9b5d1a6ac
Thomas Porter <thomasporter2019@gmail.com>
2020-09-10 21:49:38 -0400

    Add files via upload

--- tic_viz.py
+++ tic_viz.py
@@ -5,40 +5,41 @@ def won(taken):
 	for player in taken:
 		if "" in player: return True
 	return False
+	
+def mem(a,s):
+	for i in range(len(a)+1):
+		if a[0:i] in s: return True
+	return False
 		
 def compute(player_taken, all_taken, victory, n):
 	winners = [{'1','2','3'},{'4','5','6'},{'7','8','9'},{'1','4','7'},{'2','5','8'},{'3','6','9'},{'1','5','9'},{'3','5','7'}]
 	prefix = victory[:-1]
 	
-	
-		
 	# check if it completes a win
 	for winner in winners:
 		valid = True
 		for i in winner:
-			if prefix + i not in player_taken: valid = False
-		if valid and prefix not in player_taken:
+			if not mem(prefix + i, player_taken): valid = False
+		if valid and not mem(prefix, player_taken):
 			return (prefix, False)
 			
 	# check if it completes a draw
 	draw = True
 	for i in range(1,10):
-		if prefix + str(i) not in all_taken: draw = False
-	if draw and prefix not in all_taken:
+		if not mem(prefix + str(i), all_taken): 
+			draw = False
+	if draw and not mem(prefix, all_taken):
 		return (prefix, True)
 			
 	return (None, False)
 	
-def conflicting(move, propert, n):
-	for i in range(n+1):
-		if move[0:i] in propert: return True
-	return False
+def conflicting(move, taken, n):
+	return mem(move, taken)
 	
 def possible(prefix, taken, n):
 	for i in range(len(prefix)+1):
-		if prefix[0:i] in taken: return False
-	if len(prefix) == n:
-		return True
+		if prefix[0:i] in taken:return False
+	if len(prefix) == n: return True
 	assert len(prefix) <= n 
 	if len(prefix) < n:
 		for i in range(1,10):
@@ -60,7 +61,7 @@ def string_of(i, j, n):
 	
 	return re	
 		
-def tic_game(n = 5, bot = True, bot_visible = False):
+def tic_game(n = 4, bot = True, bot_visible = False):
 	
 	screenwidth = 1000
 	width = 2*sum([3**i for i in range(n)])+3**n
@@ -106,8 +107,7 @@ def tic_game(n = 5, bot = True, bot_visible = False):
 	display_hover = True
 	hover_factor = 0.30
 	
-	draw_track = False
-	prune_leaves = False
+	prune_leaves = True
 	wait_time = 0
 	
 	current = ""
@@ -185,29 +185,25 @@ def tic_game(n = 5, bot = True, bot_visible = False):
 					if bot_visible or not bot: 
 						fresh.add(victory)
 					if prune_leaves: 
-						if draw_track:
-							drawn_pruned = {i for i in drawn_pruned if i[:len(victory)] != victory}
+						drawn_pruned = {i for i in drawn_pruned if i[:len(victory)] != victory}
 						taken_pruned[0] = {i for i in taken_pruned[0] if i[:len(victory)] != victory}
 						taken_pruned[1] = {i for i in taken_pruned[1] if i[:len(victory)] != victory}
 					if draw: 
-						if draw_track:
-							drawn_pruned.add(victory)
-							drawn.add(victory)
-					else: 
+						drawn_pruned.add(victory)
+						drawn.add(victory)
+					else:
 						additions.append(victory)
 						taken_pruned[player].add(victory)
 						taken[player].add(victory)
 						
 					victory, draw = compute(taken_pruned[player], taken_pruned[0].union(taken_pruned[1]).union(drawn_pruned), victory, n)
-	
+
 				old_current = current							
 				current = additions[-1]
 				if len(current) == 0: winner = player
 				else:
 					if len(current) == 1: 
-						# version 1
 						current = ""
-						# version 2
 						if len(additions) > 1:
 							current = additions[-2]
 							
@@ -218,8 +214,7 @@ def tic_game(n = 5, bot = True, bot_visible = False):
 						if current == "": 
 							winner = -1
 							break
-						else: 
-							current = current[:-1]
+						else: current = current[:-1]
 					
 				player = 1-player
 				round_number += 1
@@ -235,7 +230,7 @@ def tic_game(n = 5, bot = True, bot_visible = False):
 					else: pygame.display.set_caption("Player "+str(winner)+" wins!")
 					mainloop = False
 				else:
-					message = "Turn "+str(round_number)+". Player "+str(player)+"'s turn. "+current
+					message = "Turn "+str(round_number)+". Player "+str(player)+"'s turn."
 					pygame.display.set_caption(message)
 	
 	if timing:
